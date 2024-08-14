788a6c6e09446756095981d3f0cdc66ab67a9f06
chrin05 <rinring105@gmail.com>
2024-08-14 21:13:19 +0900

    1-3 JPA 기초 실습(2) 진행중

--- rinny/customer-exam/src/main/java/io/chaerin/jpa/entity/Customer.java
+++ rinny/customer-exam/src/main/java/io/chaerin/jpa/entity/Customer.java
@@ -1,6 +1,13 @@
 package io.chaerin.jpa.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "customer_tb")
 public class Customer {
+    @Id
     private String id;
     private String name;
     private long registerDate;
@@ -9,6 +16,10 @@ public class Customer {
         this.id = id;
         this.name = name;
         this.registerDate = System.currentTimeMillis();
+    }
+
+    public Customer() {
+
     }
 
     public static Customer sample(){
